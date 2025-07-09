e00dcbd02a3d8ca32264034729fc2ee0ba8fb69c
tahayvr <paprikataha@gmail.com>
2025-07-09 01:31:30 -0700

    change lazyvim dash colors

--- matte-black/theme/neovim.lua
+++ matte-black/theme/neovim.lua
@@ -6,5 +6,25 @@ return {
 		opts = {
 			colorscheme = "carbonfox",
 		},
+		init = function()
+			-- Custom highlight groups for dashboard colors only
+			vim.api.nvim_create_autocmd("ColorScheme", {
+				pattern = "*",
+				callback = function()
+					vim.api.nvim_set_hl(0, "SnacksDashboardHeader", { fg = "#8a8a8d", bold = true }) 
+					vim.api.nvim_set_hl(0, "SnacksDashboardIcon", { fg = "#8a8a8d" }) 
+					vim.api.nvim_set_hl(0, "SnacksDashboardDir", { fg = "#8a8a8d" }) 
+					vim.api.nvim_set_hl(0, "SnacksDashboardFile", { fg = "#8a8a8d" }) 
+					vim.api.nvim_set_hl(0, "SnacksDashboardFooter", { fg = "#8a8a8d", italic = true })
+					vim.api.nvim_set_hl(0, "SnacksDashboardKey", { fg = "#ffffff", bold = true })
+					vim.api.nvim_set_hl(0, "SnacksDashboardDesc", { fg = "#ffffff" }) 
+				end,
+			})
+			
+			-- Apply colors immediately
+			vim.schedule(function()
+				vim.cmd("doautocmd ColorScheme")
+			end)
+		end,
 	},
 }
